e531a97f9bb4b91237f6769a8bbfdb823a3b2f4c
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2026-04-22 10:44:39 -0300

    feat(home): add clipboard button to alias

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -56,9 +56,17 @@
                                 <tr :key="$link->id">
                                     <td class="font-bold">{{ $link->title }}</td>
                                     <td>
-                                        <a href="{{ url($link->alias) }}" target="_blank" class="link link-secondary">
-                                            {{ $link->aliasLabel() }}
-                                        </a>
+                                        <div x-data="clipboard('{{ url($link->alias) }}')" class="flex items-center gap-2">
+                                            <a href="{{ url($link->alias) }}" target="_blank" class="link link-secondary">
+                                                {{ $link->aliasLabel() }}
+                                            </a>
+                                            <button class="btn btn-ghost btn-xs btn-square text-base-content/30 hover:text-primary tooltip"
+                                                :data-tip="copied ? 'Link copied' : 'Copy link'" x-on:click="copy">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
+                                                </svg>
+                                            </button>
+                                        </div>
                                     </td>
                                     <td class="font-bold">{{ $link->visits->count() }}</td>
                                 </tr>
