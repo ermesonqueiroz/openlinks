1a7480689bba48941c26911fb306a34c5a0fa3d2
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2026-04-22 10:44:01 -0300

    fix: clipboard alpine component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,18 @@
-import Alpine from 'alpinejs';
 import { Chart, registerables } from 'chart.js';
 
 Chart.register(...registerables);
 window.Chart = Chart;
 
-Alpine.data('clipboard', (textToCopy = '') => ({
-    copied: false,
-    content: textToCopy,
-    copy() {
-        if (!this.content) return;
-        navigator.clipboard.writeText(this.content).then(() => {
-            this.copied = true;
-            setTimeout(() => this.copied = false, 2000);
-        });
-    }
-}));
-
-window.Alpine = Alpine;
-Alpine.start();
+document.addEventListener('alpine:init', () => {
+    window.Alpine.data('clipboard', (textToCopy = '') => ({
+        copied: false,
+        content: textToCopy,
+        copy() {
+            if (!this.content) return;
+            navigator.clipboard.writeText(this.content).then(() => {
+                this.copied = true;
+                setTimeout(() => this.copied = false, 2000);
+            });
+        }
+    }));
+});
